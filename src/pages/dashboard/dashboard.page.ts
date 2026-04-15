548ad04af3ed9b08b04476f866d0f1a179264092
Manuel Diaz <manueldl12062003@gmail.com>
2026-04-15 02:00:29 +0200

    feat: created base for the dashboard waiting for dependencies to continue

---
+++ src/pages/dashboard/dashboard.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './dashboard.page.html',
+  styleUrl: './dashboard.page.css',
+})
+export class DashboardPage {
+
+}
