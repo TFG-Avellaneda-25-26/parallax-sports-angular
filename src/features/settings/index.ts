03380fe35e19e758560d00107a0d280c9265746b
Manuel Diaz <manueldl12062003@gmail.com>
2026-04-26 01:13:18 +0200

    feat: added auto-complete component

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -2,3 +2,4 @@ export { AccountComponent } from './ui/account/account.component';
 export { PreferencesComponent } from './ui/preferences/preferences.component';
 export { FollowsComponent } from './ui/follows/follows.component';
 export { AdminComponent } from './ui/admin/admin.component';
+export { AutocompleteComponent } from './ui/auto-complete/auto-complete.component';
